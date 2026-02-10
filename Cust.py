07086c7d07ff893c6ff6ac1a6bc34ad44354a177
Karthikprasanthb <152365747+Karthikprasanthb@users.noreply.github.com>
2026-02-10 11:09:51 +0530

    Update Cust.py

--- Cust.py
+++ Cust.py
@@ -55,17 +55,8 @@ DATA_FILE = "Sales_Data3.xlsx"
 
 
 TARGET_CUSTOMERS = [
-    "TechnipFMC Norge AS (KOS1)",
-    "TECHNIPFMC INDUSTRIES-SOLE PROPRIETORSHIP L.L.C. (7042)",
-    "TechnipFMC Canada Ltd (EWOD)",
-    "TechnipFMC Services Australia Ltd (EWAA)",
-    "TechnipFMC do Brasil Ltda (REMS - ESPB - Fassub)",
-    "TechnipFMC do Brasil Ltda (Macaé - EWBO)",
-    "TechnipFMC do Brasil Ltda (Fassub - EWBO)",
-    "TechnipFMC Services Australia Ltd (SFVA)",
-    "Stream-Flo Edmonton",
-    "Stream-Flo USA LLC",
-    "QUEST SEALING SOLUTION"
+   "ABCD SOLUTION",
+    "ALONE SOLUTION "
 ]
 
 # =====================================================
@@ -459,5 +450,6 @@ if not monthly_summary.empty:
     reflects **project-based and non-uniform procurement behavior**
     rather than consistent monthly ordering.
     """)
+
 
 
